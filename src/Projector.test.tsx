fd0c61241f9d4b8055ce22658e7241c16485a4de
Anthony Gonzales <antgonzales@icloud.com>
2022-06-05 14:07:47 -0400

    Add getTabProps and getSlideProps
    Add ability to change slides. Add ARIA compliant roles.

--- src/Projector.test.tsx
+++ src/Projector.test.tsx
@@ -1,15 +1,115 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import Projector from ".";
 
 describe("<Projector />", () => {
-  it("renders children", () => {
-    render(
-      <Projector>
-        <div>Hello</div>
-      </Projector>
-    );
-    expect(screen.getByText("Hello")).toBeInTheDocument();
+  describe("getTabProps()", () => {
+    it("provides an ARIA role", () => {
+      const slides = [
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+      ];
+      render(
+        <Projector>
+          {({ getTabProps }) => {
+            return slides.map((slide, index) => (
+              <button
+                {...getTabProps({
+                  key: index + "-kitten-btn",
+                })}
+              >
+                Slide {index}
+              </button>
+            ));
+          }}
+        </Projector>
+      );
+      expect(screen.getAllByRole("tab").length).toBe(slides.length);
+    });
+
+    it("display the currently active tab by slide", () => {
+      const slides = [
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+      ];
+      render(
+        <Projector>
+          {({ getTabProps }) => {
+            return slides.map((slide, index) => (
+              <button
+                {...getTabProps({
+                  index,
+                  key: index + "-kitten-btn",
+                })}
+              >
+                Slide {index}
+              </button>
+            ));
+          }}
+        </Projector>
+      );
+      expect(screen.getByRole("tab", { selected: true }).textContent).toBe(
+        "Slide 0"
+      );
+    });
+
+    it("changes the active side index on click", () => {
+      const slides = [
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+      ];
+      render(
+        <Projector>
+          {({ getTabProps }) => {
+            return slides.map((slide, index) => (
+              <button
+                {...getTabProps({
+                  index,
+                  key: index + "-kitten-btn",
+                })}
+              >
+                Slide {index}
+              </button>
+            ));
+          }}
+        </Projector>
+      );
+      const nextSlide = screen.getAllByRole("tab", { selected: false })[0];
+
+      fireEvent.click(nextSlide);
+
+      expect(screen.getByRole("tab", { selected: true }).textContent).toBe(
+        "Slide 1"
+      );
+    });
+  });
+
+  describe("getSlideProps()", () => {
+    it("adds an ARIA compliant role", () => {
+      const slides = [
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+        { img: "http://placekitten.com/200/200" },
+      ];
+      render(
+        <Projector>
+          {({ getSlideProps }) => {
+            return slides.map((slide, index) => (
+              <img
+                {...getSlideProps({
+                  key: index + "-kitten-img",
+                  src: slide.img,
+                })}
+              />
+            ));
+          }}
+        </Projector>
+      );
+      expect(screen.getAllByRole("tabpanel").length).toBe(slides.length);
+    });
   });
 });
--- src/Projector.tsx
+++ src/Projector.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Projector({ children }) {
-  return children;
+  const [activeSlideIndex, setActiveSlideIndex] = React.useState(0);
+  const getTabProps = ({ index, ...rest }) => {
+    return {
+      role: "tab",
+      "aria-selected": activeSlideIndex === index,
+      onClick: () => {
+        setActiveSlideIndex(index);
+      },
+      ...rest,
+    };
+  };
+  const getSlideProps = ({ ...rest }) => {
+    return { role: "tabpanel", "aria-roledescription": "slide", ...rest };
+  };
+  return children({ getSlideProps, getTabProps });
 }
